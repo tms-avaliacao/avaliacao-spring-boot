ce9633c5db79ced43a010031b080d12ff58ab10c
dnamobile <dnamobile@users.noreply.github.com>
2020-05-03 18:55:01 -0300

    Correção de bugs

--- src/main/java/br/com/tokiomarine/seguradora/avaliacao/controller/EstudanteController.java
+++ src/main/java/br/com/tokiomarine/seguradora/avaliacao/controller/EstudanteController.java
@@ -55,7 +55,7 @@ public class EstudanteController {
 			return "atualizar-estudante";
 		}
 
-		estudante.setId(id);
+//		estudante.setId(id);
 		service.atualizarEstudante(estudante);
 		return "redirect:/estudantes/listar";
 	}
--- src/main/java/br/com/tokiomarine/seguradora/avaliacao/entidade/Estudante.java
+++ src/main/java/br/com/tokiomarine/seguradora/avaliacao/entidade/Estudante.java
@@ -16,7 +16,7 @@ import javax.validation.constraints.NotNull;
 public class Estudante {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
 	@Column(nullable = false)
